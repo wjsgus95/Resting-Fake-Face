b5508cbae29cf487bea330b96242a599c515649a
wjsgus95 <wjsgus95@github.com>
2019-05-23 22:28:58 +0900

    base structure

---
+++ common/constants.py
@@ -0,0 +1,7 @@
+
+INPUT_SIZE = 128
+
+trainset_dir = "./trainset"
+fake_dir = trainset_dir + "/Fake"
+true_dir = trainset_dir + "/True"
+gan_dir  = trainset_dir + "/gan"
---
+++ util/downsampler.py
@@ -0,0 +1,11 @@
+
+import cv2
+
+# Only for square images
+class Downsampler():
+    def __init__(self):
+        pass
+
+    def get_size(self, size):
+        pass
+        return new_img
