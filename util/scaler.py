5e5df62db50d28af09fdcbbf33e85aca9f81cba6
mtegene <mtegene@andrew.cmu.edu>
2019-05-24 00:21:20 +0900

    the basic resizing of the images, no pyrDown yet

--- util/scaler.py
+++ util/scaler.py
@@ -1,4 +1,4 @@
-
+import os
 import cv2
 
 # Only for square images
@@ -6,6 +6,30 @@ class Scalar():
     def __init__(self):
         pass
 
-    def get_size(self, size):
-        pass
-        return new_img
+    def get_img_of_size(self, img, size):
+        return cv2.pyrDown(img, size)
+
+    def convert_to_size(self, folder, newfolder, size):
+        if not os.path.exists(newfolder):
+            os.makedirs(newfolder)  
+        
+        #count = 1 #debugging
+        for filename in os.listdir(folder):
+            # count += 1
+            # if (count > 10):
+            #     break  #debugging
+
+            img = cv2.imread(os.path.join(folder, filename))
+            if img is not None:
+                new_img = cv2.resize(img, dsize=(size, size))
+                cv2.imwrite(newfolder + "/" + filename, new_img)
+
+if __name__ == "__main__":
+    scaler = Scalar()
+
+    size = 128
+    scaler.convert_to_size('trainset/fake', 'trainset/fake_new_images', size)
+    scaler.convert_to_size('trainset/gan', 'trainset/gan_new_images', size)
+    scaler.convert_to_size('trainset/real', 'trainset/real_new_images', size)
+
+    pass
