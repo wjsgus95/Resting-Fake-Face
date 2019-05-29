6914a5e4fe3e26b9454b519ac85155357610fa8b
mtegene <mtegene@andrew.cmu.edu>
2019-05-29 18:31:58 +0900

    alexnet with tweaked numbers, only 60% accurate

--- architectures/alex_net.py
+++ architectures/alex_net.py
@@ -10,7 +10,8 @@ import torch.optim as optim
 #from common.constants import *
 INPUT_SIZE = 128
 
-TRAINSET_DIR = "./uniform_trainset"
+TRAINSET_DIR = "./uniform_trainset_diff"
+TESTSET_DIR = "./testset"
 FAKE_DIR = TRAINSET_DIR + "/Fake"
 TRUE_DIR = TRAINSET_DIR + "/True"
 GAN_DIR  = TRAINSET_DIR + "/gan"
@@ -21,7 +22,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameters
 num_epochs = 10
-batch_size = 100
+batch_size = 128
 learning_rate = 0.001
 loss_func = nn.CrossEntropyLoss()
 # Optimize function needs nn.Module instance on construction, hence callback function is used.
@@ -31,13 +32,29 @@ optimize_getter = lambda m, m_lr: optim.Adam(m.parameters(), lr=m_lr)
 #train_dataset = torchvision.datasets.FashionMNIST("./", train=True, transform=transforms.ToTensor(), download=True)
 train_dataset = torchvision.datasets.ImageFolder( root=TRAINSET_DIR, transform=torchvision.transforms.ToTensor())
 #test_dataset = torchvision.datasets.FashionMNIST("./", train=False, transform=transforms.ToTensor())
-test_dataset = torchvision.datasets.ImageFolder( root=TRAINSET_DIR, transform=torchvision.transforms.ToTensor())
+test_dataset = torchvision.datasets.ImageFolder( root=TESTSET_DIR, transform=torchvision.transforms.ToTensor())
 
 # Data loader
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
 
 #more params
+"""
+Stucture
+CONV1: in_channels=3, out_channels=96, kernel_size=12, stride=2, padding=0, bias=True
+MAXPOOL1: kernel_size=3, stride=2
+NORM1
+CONV2: in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2, bias=True
+MAXPOOL2: kernel_size=3, stride=2
+NORM2
+CONV3: in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1, bias=True
+CONV4: in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1, bias=True
+CONV5: in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1, bias=True
+MaxPOOL3: kernel_size=4, stride=2
+FC6: 8136, 4096
+FC7: 4096, 1000
+FC8: 1000, 2
+"""
 k_s = 5
 
 # Resting Fake Face Model
@@ -46,26 +63,26 @@ class AlexNet(torch.nn.Module):
         super(AlexNet, self).__init__()
         self.relu = nn.ReLU()
 
-        self.conv1 = torch.nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1, padding=2, bias=True)
-        self.max_pool1 = torch.nn.MaxPool2d(kernel_size=2)
-        self.batchnorm1 = nn.BatchNorm2d(6)
-        self.conv2 = torch.nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0, bias=True)
-        self.max_pool2 = torch.nn.MaxPool2d(kernel_size=2)
-        self.batchnorm2 = nn.BatchNorm2d(16)
+        self.conv1 = torch.nn.Conv2d(in_channels=3, out_channels=96, kernel_size=12, stride=2, padding=0, bias=True)
+        self.max_pool1 = torch.nn.MaxPool2d(kernel_size=3, stride=2)
+        self.batchnorm1 = nn.BatchNorm2d(96)
+        self.conv2 = torch.nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2, bias=True)
+        self.max_pool2 = torch.nn.MaxPool2d(kernel_size=3, stride=2)
+        self.batchnorm2 = nn.BatchNorm2d(256)
         
         #add in 3 more conv layers
-        self.conv3 = torch.nn.Conv2d(in_channels=16, out_channels=24, kernel_size=k_s, stride=1, padding=2, bias=True)
-        self.conv4 = torch.nn.Conv2d(in_channels=24, out_channels=32, kernel_size=k_s, stride=1, padding=2, bias=True)
-        self.conv5 = torch.nn.Conv2d(in_channels=32, out_channels=48, kernel_size=k_s, stride=1, padding=2, bias=True)
-        self.max_pool3 = torch.nn.MaxPool2d(kernel_size=2)
+        self.conv3 = torch.nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv4 = torch.nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv5 = torch.nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1, bias=True)
+        self.max_pool3 = torch.nn.MaxPool2d(kernel_size=4, stride=2)
 
-        # self.fc1 = torch.nn.Linear(48*5*5, 600)
-        # self.fc2 = torch.nn.Linear(600, 120)
-        # self.fc3 = torch.nn.Linear(128, 2)
+        self.fc1 = torch.nn.Linear(9216, 4096)
+        self.fc2 = torch.nn.Linear(4096, 1000)
+        self.fc3 = torch.nn.Linear(1000, 2)
 
-        self.fc1 = torch.nn.Linear(16*30*30, 4200)
-        self.fc2 = torch.nn.Linear(4200, 800)
-        self.fc3 = torch.nn.Linear(800, 3)
+        # self.fc1 = torch.nn.Linear(16*30*30, 4200)
+        # self.fc2 = torch.nn.Linear(4200, 800)
+        # self.fc3 = torch.nn.Linear(800, 3)
 
         #torch.save(self, ckpt_path)
 
@@ -83,16 +100,17 @@ class AlexNet(torch.nn.Module):
         x = self.batchnorm2(x)
 
         #three rounds of conv
-        # x = self.conv3(x)
-        # x = self.relu(x)
-        # x = self.conv4(x)
-        # x = self.relu(x)
-        # x = self.conv5(x)
-        # x = self.relu(x)
-        # x = self.max_pool3(x)
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.conv4(x)
+        x = self.relu(x)
+        x = self.conv5(x)
+        x = self.relu(x)
+        x = self.max_pool3(x)
 
         # Flatten
-        x = x.view(-1, 16*30*30)
+        #x = x.view(-1, 16*30*30)
+        x = x.view(-1, 9216)
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
