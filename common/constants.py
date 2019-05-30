575603339fb5783cad008db9aff2cbe1f1699416
wjsgus95 <wjsgus95@gmail.com>
2019-05-30 16:30:22 +0900

    accuracy 80%?!

--- common/constants.py
+++ common/constants.py
@@ -1,9 +1,25 @@
 
 INPUT_SIZE = 128
 
+"""
+Don't include slashes in directory naems"
+"""
+
+
+# Pretrain dataset
+PRETRAIN_DIR = "./dataset"
+PRETRIN_FAKE_DIR = PRETRAIN_DIR + "/Fake"
+PRETRIN_TRUE_DIR = PRETRAIN_DIR + "/True"
+
+
+# Given train dataset
 TRAINSET_DIR = "./uniform_trainset"
 FAKE_DIR = TRAINSET_DIR + "/Fake"
 TRUE_DIR = TRAINSET_DIR + "/True"
 GAN_DIR  = TRAINSET_DIR + "/gan"
 
+# Given test dataset
+TESTSET_DIR = "./sample"
 
+# Checkpoint directory
+CKPT_DIR = "checkpoints"
--- main.py
+++ main.py
@@ -13,22 +13,22 @@ from common.constants import *
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameters
-num_epochs = 10
-batch_size = 100
-learning_rate = 0.001
+num_epochs = 30
+batch_size = 16
+learning_rate = 0.004
 loss_func = nn.CrossEntropyLoss()
 # Optimize function needs nn.Module instance on construction, hence callback function is used.
 optimize_getter = lambda m, m_lr: optim.Adam(m.parameters(), lr=m_lr)
 
 # Dataset
-#train_dataset = torchvision.datasets.FashionMNIST("./", train=True, transform=transforms.ToTensor(), download=True)
 train_dataset = torchvision.datasets.ImageFolder( root=TRAINSET_DIR, transform=torchvision.transforms.ToTensor())
-#test_dataset = torchvision.datasets.FashionMNIST("./", train=False, transform=transforms.ToTensor())
-test_dataset = torchvision.datasets.ImageFolder( root=TRAINSET_DIR, transform=torchvision.transforms.ToTensor())
+test_dataset = torchvision.datasets.ImageFolder( root=TESTSET_DIR, transform=torchvision.transforms.ToTensor())
+#train_dataset = torchvision.datasets.ImageFolder( root=PRETRAIN_DIR, transform=torchvision.transforms.ToTensor())
+#test_dataset = torchvision.datasets.ImageFolder( root=TRAINSET_DIR, transform=torchvision.transforms.ToTensor())
 
 # Data loader
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
 
 # Resting Fake Face Model
 class RFF(torch.nn.Module):
@@ -39,54 +39,107 @@ class RFF(torch.nn.Module):
         self.softmax = nn.Softmax(dim=1)
 
         """
-        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2, bias=True)
-        self.max_pool1 = torch.nn.MaxPool2d(kernel_size=2)
-        self.conv2 = torch.nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0, bias=True)
-        self.max_pool2 = torch.nn.MaxPool2d(kernel_size=2)
-        self.fc1 = torch.nn.Linear(16*5*5, 120)
-        self.fc2 = torch.nn.Linear(120, 84)
-        self.fc3 = torch.nn.Linear(84, 10)
-        """
-
         # Block 1
-        self.conv1 = torch.nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1, bias=True)
-        self.conv2 = torch.nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1, bias=True)
-        self.conv3 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1, bias=True)
-        self.max_pool1 = torch.nn.MaxPool2d(kernel_size=4)
+        self.conv1 = torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv2 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv3 = torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1, bias=True)
+        self.max_pool1 = torch.nn.MaxPool2d(kernel_size=2)
 
         # Block 2
-        self.conv4 = torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1, bias=True)
-        self.conv5 = torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1, bias=True)
-        self.conv6 = torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1, bias=True)
-        self.max_pool2 = torch.nn.MaxPool2d(kernel_size=4)
+        self.conv4 = torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv5 = torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv6 = torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=True)
+        self.max_pool2 = torch.nn.MaxPool2d(kernel_size=2)
 
         # Block 3
-        self.conv7 = torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1, bias=True)
-        self.conv8 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv7 = torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv8 = torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=True)
+        self.max_pool3 = torch.nn.MaxPool2d(kernel_size=2)
 
         # Block 4
-        self.fc1 = torch.nn.Linear(32768, 256)
-        self.batchnorm = nn.BatchNorm1d(256)
-        self.fc2 = torch.nn.Linear(256, 3)
+        #self.conv9  = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1, bias=True)
+        #self.conv10 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1, bias=True)
+        #self.max_pool4 = torch.nn.MaxPool2d(kernel_size=2)
+
+        # Block 5
+        self.fc1 = torch.nn.Linear(256 * 16 * 16, 2048)
+        #self.fc2 = torch.nn.Linear(2048, 2048)
+        self.batchnorm = nn.BatchNorm1d(2048)
+        self.fc2 = torch.nn.Linear(2048, 2)
+        """
+        # Block 1
+        self.conv1 = torch.nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=0, bias=True)
+        self.conv2 = torch.nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=0, bias=True)
+        self.conv3 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=0, bias=True)
+        self.max_pool1 = torch.nn.MaxPool2d(kernel_size=2)
+
+        # Block 2
+        self.conv4 = torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=0, bias=True)
+        self.conv5 = torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=0, bias=True)
+        self.conv6 = torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=0, bias=True)
+        self.max_pool2 = torch.nn.MaxPool2d(kernel_size=2)
+
+        # Block 3
+        self.conv7 = torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=0, bias=True)
+        self.conv8 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=0, bias=True)
+
+        # Block 4
+        #self.fc1 = torch.nn.Linear(32768, 1024)
+        self.fc1 = torch.nn.Linear(270848, 1024)
+        self.batchnorm = nn.BatchNorm1d(1024)
+        self.fc2 = torch.nn.Linear(1024, 2)
+        #self.fc3 = torch.nn.Linear(1024, 2)
 
         #torch.save(self, ckpt_path)
 
     def forward(self, x):
+
         """
+        # Block 1
         x = self.conv1(x)
         x = self.relu(x)
-        x = self.max_pool1(x)
+        x = self.dropout(x)
         x = self.conv2(x)
         x = self.relu(x)
+        x = self.dropout(x)
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.max_pool1(x)
+        x = self.dropout(x)
+
+        # Block 2
+        x = self.conv4(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.conv5(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.conv6(x)
+        x = self.relu(x)
         x = self.max_pool2(x)
-        # Flatten
-        x = x.view(-1, 16*5*5)
+        x = self.dropout(x)
+
+        # Block 3
+        x = self.conv7(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.conv8(x)
+        x = self.relu(x)
+        x = self.max_pool3(x)
+        x = self.dropout(x)
+
+        # Block 4
+        x = x.view(-1, 256 * 16 * 16)
         x = self.fc1(x)
         x = self.relu(x)
+        x = self.batchnorm(x)
+        x = self.dropout(x)
         x = self.fc2(x)
         x = self.relu(x)
         x = self.fc3(x)
+        x = self.softmax(x)
         """
+
         # Block 1
         x = self.conv1(x)
         x = self.relu(x)
@@ -120,34 +173,41 @@ class RFF(torch.nn.Module):
         x = self.dropout(x)
 
         # Block 4
-        print(x.shape)
-        x = x.view(-1, 32768)
-        print(x.shape)
+        x = x.view(-1, 270848)
         x = self.fc1(x)
-        print(x.shape)
         x = self.relu(x)
-        print(x.shape)
         x = self.batchnorm(x)
-        print(x.shape)
         x = self.dropout(x)
         x = self.fc2(x)
+        #x = self.relu(x)
+        #x = self.fc3(x)
         x = self.softmax(x)
 
         return x
 
     def train(self):
+        ckpt_path_base = CKPT_DIR + "/shallownet_v3"
+
         optimizer = optimize_getter(self, learning_rate)
 
         # Train the model
         total_step = len(train_loader)
         for epoch in range(num_epochs):
+            total = 0
+            correct = 0
             for i, (images, labels) in enumerate(train_loader):
+
                 # Move tensors to the configured device
                 images = images.to(device)
                 labels = labels.to(device)
 
                 # Forward pass
                 outputs = self(images)
+
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
                 # If L1 loss function is applied, do one hot encoding.
                 if type(loss_func) == type(nn.L1Loss()):
                     labels = F.one_hot(labels, num_classes=10)
@@ -159,8 +219,13 @@ class RFF(torch.nn.Module):
                 loss.backward()
                 optimizer.step()
 
-                if (i+1) % 100 == 0:
-                    print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+                if (i+1) % 10 == 0:
+                    print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.4f}%'.format(epoch+1, num_epochs, i+1, total_step, loss.item(), 100*correct/total))
+                    total = 0
+                    correct = 0
+            if (epoch + 1) % 10 == 0:
+                torch.save(self, ckpt_path_base + f"_{epoch+1}.ckpt")
 
     def test(self):
         # Test the model
@@ -175,10 +240,13 @@ class RFF(torch.nn.Module):
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
-            print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
+            print(f'Accuracy of the network on the {total} test images: {100 * correct / total} %')
 
 
 if __name__ == "__main__":
-    rff = RFF().to(device)
-    rff.train()
+    #rff = RFF().to(device)
+    #rff.train()
+    ckpt_path = CKPT_DIR + "/shallownet_v3_20.ckpt"
+    rff = torch.load(ckpt_path).to(device)
+    rff.test()
 
